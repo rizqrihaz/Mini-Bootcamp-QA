8af55ac44c28e923b8a99ea47f0c2364afaf2a54
Riz <118885881+RizqRihaz13@users.noreply.github.com>
2026-04-15 22:26:54 +0700

    Add files via upload

---
+++ Sesi 4_Homework/menu.js
@@ -0,0 +1,32 @@
+import { kalkulator } from './rumus.js';
+import readline from 'readline';
+
+// Membuat interface untuk input dari terminal
+const inputUser = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Input angka pertama
+inputUser.question('Masukkan angka pertama: ', angka1 => {
+
+    // Input angka kedua
+    inputUser.question('Masukkan angka kedua: ', angka2 => {
+
+        // Input operator
+        inputUser.question('Masukkan operator (+, -, *, /): ', operator => {
+
+            console.log(
+                `Hasil: ${kalkulator(
+                    parseFloat(angka1),
+                    parseFloat(angka2),
+                    operator
+                )}`
+            );
+
+            inputUser.close();
+        });
+
+    });
+
+});
